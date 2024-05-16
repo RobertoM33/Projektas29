9ef7d13d368dccfe6c96cc0f342c4b42865bc119
Robis <Robis@WKS-1BN-13>
2024-05-16 14:44:06 +0300

    Add project files.

---
+++ Projektas29/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DvidesimtDevinmtassProjektas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+
+        /*Užduotis 29: While*/
+        {
+            int n = 0;
+            Console.WriteLine($" Iveskite skaiciu");
+            int Skaicius = int.Parse(Console.ReadLine());
+            int Ilgis = Skaicius.ToString().Length;
+            Console.WriteLine($"Ilgis {Ilgis}")
+            ;
+            {
+               while (n< Skaicius)
+
+                      Console.WriteLine(n);
+                  n--;
+              }
+              Console.WriteLine($"Pirminis {Ilgis}");
+
+
+            }
+        }
+    }
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
